6f36dd207ce4ed9ece465fc479410a95ee5f03a0
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2022-07-12 16:19:31 -0400

    Show tooltips

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -6,6 +6,13 @@ class ProjectItem {
         this.node = document.getElementById(this.id);
         this.connectAction();
         this.connectMoreInfo();
+
+        // add tooltip
+        this.info = this.node.getAttribute("data-extra-info");
+        const tooltip = document.createElement("div");
+        tooltip.setAttribute("class", "tooltip");
+        tooltip.innerText = this.info;
+        this.node.append(tooltip);
     }
 
     connectAction() {
@@ -24,7 +31,26 @@ class ProjectItem {
         this.action = this.action === "Activate" ? "Finish" : "Activate";
     }
 
-    connectMoreInfo() {}
+    connectMoreInfo() {
+        const button = this.node.querySelector(".alt");
+        button.addEventListener("click", () => {
+            const tooltip = this.node.querySelector(".tooltip");
+            if (tooltip.style.opacity === "0" || !tooltip.style.opacity) {
+                tooltip.style.opacity = "1";
+                button.classList.add("button-clicked");
+
+                document.addEventListener("click", (event) => {
+                    if (!button.contains(event.target)) {
+                        tooltip.style.opacity = "0";
+                        button.classList.remove("button-clicked");
+                    }
+                });
+            } else {
+                tooltip.style.opacity = "0";
+                button.classList.remove("button-clicked");
+            }
+        });
+    }
 }
 
 export default ProjectItem;
